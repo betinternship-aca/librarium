a4dff96a2bc880b0cf0d394cdadafa29b5637872
hospogh <hospogh@gmail.com>
2017-07-29 05:16:26 +0400

    @hospogh *CREATE Order class with all methods

--- src/server-api/index.ts
+++ src/server-api/index.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
 import {UserRouter} from './user';
 import {BookRouter} from './book';
+import {OrderRouter} from './order';
 
 export const ApiRouter = express.Router();
 
 ApiRouter.use('/user', UserRouter);
 ApiRouter.use('/book', BookRouter);
-
+ApiRouter.use('/order', OrderRouter);
