f10c8181f6bd96ab9821f891d6aa567c86f8e4d3
irina <i.mkhitaryan8@mail.ru>
2017-08-02 11:33:30 +0400

    CREATE services

---
+++ src/app/org/services/org.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class OrgService {
+
+  constructor(private http: HttpClient) { }
+
+  createOrg(data) {
+    return this.http.post('/api/org/', data);
+  }
+}
